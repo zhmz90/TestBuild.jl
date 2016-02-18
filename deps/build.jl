993b98df4662b819184d51c61a102aece84e9c67
zhmz90 <zhmz90@gmail.com>
2016-02-18 18:02:51 +0800

    install autoconf from a tarball

--- deps/build.jl
+++ deps/build.jl
@@ -13,26 +13,53 @@ if !detecthts()
     #provides(Sources, Dict(URI("http://zlib.net/zlib-1.2.8.tar.gz") => zlib))
     
     # Build libhts
-    
 
-    @osx_only begin
-        autoconf = library_dependency("autoconf", os=:Darwin)
-        if Pkg.installed("Homebrew") === nothing
-            error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
-        end
-        provides( Homebrew.HB, "autoconf", autoconf, os = :Darwin )
-        @BinDeps.install Dict(:autoconf => :autoconf)
-    end
-    
     hts = library_dependency("hts", aliases=["libhts","libhts.so","libhts.dylib","libhts.dll"])#, runtime=true, os=:Unix)
     _prefix = joinpath(BinDeps.depsdir(hts),"usr")
     _srcdir = joinpath(BinDeps.depsdir(hts),"src")
     _htsdir = joinpath(_srcdir,"htslib-1.3")
     _libdir = joinpath(_prefix, "lib")
+
+    @osx_only begin
+        autoconf = library_dependency("autoconf", os=:Darwin)
+        #=
+        if Pkg.installed("Homebrew") === nothing
+            error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+        end
+        provides( Homebrew.HB, "autoconf", autoconf, os = :Darwin )
+        =#
+        
+        _prefix = joinpath(BinDeps.depsdir(hts),"usr")
+        _srcdir = joinpath(BinDeps.depsdir(hts),"src")
+        _autoconfdir = joinpath(_srcdir,"autoconf-2.68")
+        _bindir = joinpath(_prefix, "bin")
+        
+        provides(BuildProcess,
+                 (@build_steps begin
+                  CreateDirectory(_srcdir)
+                  CreateDirectory(_bindir)
+                  @build_steps begin
+                  ChangeDirectory(_srcdir)
+                  `rm -rf autoconf`
+                  `wget http://ftpmirror.gnu.org/autoconf/autoconf-2.68.tar.gz`
+                  `tar xvf autoconf-2.68.tar.gz`
+                  FileRule(joinpath(_bindir, "autoconf"), @build_steps begin
+                           ChangeDirectory(_autoconfdir)
+                           `./configure`
+                           `make`
+                           end)
+                  end
+                 end), autoconf)
+        
+        
+        @BinDeps.install Dict(:autoconf => :autoconf)
+    end
+    
+
     
     provides(BuildProcess,
              (@build_steps begin
-                CreateDirectory(_srcdir)
+                #CreateDirectory(_srcdir)
                 CreateDirectory(_libdir)
                 @build_steps begin
                   ChangeDirectory(_srcdir)
@@ -41,7 +68,8 @@ if !detecthts()
                   `tar xvf htslib-1.3.tar.bz2`
                    FileRule(joinpath(_libdir, "libhts.so"), @build_steps begin
                      ChangeDirectory(_htsdir)
-                     `autoconf`
+                     @linux_only `autoconf`
+                     @osx_only `../../bin/autoconf`
                      `./configure`
                      `make`
                      @linux_only `cp libhts.so $_libdir`
